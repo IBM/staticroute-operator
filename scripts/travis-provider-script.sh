56842ae501270ecdc758fa785b02b06dfb838db7
birotamas <tamas.biro1@ibm.com>
2020-02-24 15:57:46 +0100

    Changes the order of push and tag

--- scripts/travis-provider-script.sh
+++ scripts/travis-provider-script.sh
@@ -4,12 +4,13 @@ set -e
 script_path=$PWD/$(dirname "$0")
 
 NEXT_VERSION=v$($script_path/get-next-version-by-commit.sh "$TRAVIS_COMMIT_MESSAGE")
-git remote set-url --push origin https://${GH_TOKEN}@${GH_REPO}.git
-git tag build-${TRAVIS_BUILD_NUMBER}
-git tag ${NEXT_VERSION}
-git push origin ${TRAVIS_BRANCH} --tags
 if [[ $TRAVIS_COMMIT_MESSAGE =~ "_publish_" ]]
 then
     docker tag ${REGISTRY_URL}/${DOCKER_IMAGE_NAME} ${DOCKER_IMAGE_NAME}:${NEXT_VERSION}
     $script_path/push-docker-image.sh ${DOCKER_IMAGE_NAME} ${NEXT_VERSION}
 fi
+
+#if publish was successful, tag the commit
+git remote set-url --push origin https://${GH_TOKEN}@${GH_REPO}.git
+git tag ${NEXT_VERSION}-${TRAVIS_BUILD_NUMBER}
+git push origin ${TRAVIS_BRANCH} --tags
