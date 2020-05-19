6b16de9259ab2a0746b04b6a6e8484547c72bb6d
Tamas Biro <tamas.biro1@ibm.com>
2020-05-19 12:03:21 +0000

    use operator-sdk v0.15.x

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -47,11 +47,11 @@ import (
 	kubemetrics "github.com/operator-framework/operator-sdk/pkg/kube-metrics"
 	"github.com/operator-framework/operator-sdk/pkg/log/zap"
 	"github.com/operator-framework/operator-sdk/pkg/metrics"
-	"github.com/operator-framework/operator-sdk/pkg/restmapper"
 	sdkVersion "github.com/operator-framework/operator-sdk/version"
 	"github.com/spf13/pflag"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
@@ -169,7 +169,7 @@ func mainImpl(params mainImplParams) {
 	// Create a new Cmd to provide shared dependencies and start components
 	mgr, err := params.newManager(cfg, manager.Options{
 		Namespace:          "",
-		MapperProvider:     restmapper.NewDynamicRESTMapper,
+		MapperProvider:     apiutil.NewDiscoveryRESTMapper,
 		MetricsBindAddress: fmt.Sprintf("%s:%d", metricsHost, metricsPort),
 	})
 	if err != nil {
