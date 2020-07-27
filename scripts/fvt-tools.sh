a57dc8b5585d4d3f4f178fde07b4399635abab1a
birotamas <tamas.biro1@ibm.com>
2020-07-27 15:35:54 +0200

    Fix fvt tests on IBMCloud provider

--- scripts/fvt-tools.sh
+++ scripts/fvt-tools.sh
@@ -61,6 +61,7 @@ exec_in_hostnet_of_node() {
 }
 
 get_default_gw() {
+  local nodename=${1}
   if [[ "${PROVIDER}" == "ibmcloud" ]]; then
     provider_type=$(get_provider_type)
     [[ ${provider_type} == "softlayer" ]] && v="10.0.0.0/8" || v="default"
